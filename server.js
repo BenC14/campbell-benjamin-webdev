f78e13ecf0a96225beccc93cf864aada0f1de65f
Ben Campbell <bencampbell@Bens-MacBook-Pro-2.local>
2017-05-14 14:39:01 -0400

    adding mongodb to herkou

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 require ("./test/app.js")(app);
+var myApp = require('./lectures/afternoon/app');
 
+// console.log(myApp)
+// myApp.sayHello();
+
+myApp(app);
 var port = process.env.PORT || 3000;
 
 app.listen(port);
